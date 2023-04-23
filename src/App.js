336c359b425badd014ffda71fb6ad0f800885562
Reza Abusaidi <reza.abusaidi@aianxtec.com>
2023-04-23 17:18:54 +0100

    added Home back to directory

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import homepage from './routes/homepage'
+import Home from './routes/Home'
 
 
 function App() {
   return (
     <div>
       <Routes>
-      <Route path='/' element={<homepage />} />
+      <Route path='/' element={<Home />} />
 
       </Routes>
     </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
+
       <h1>Aianx</h1>
+      
     </div>
   )
 }
